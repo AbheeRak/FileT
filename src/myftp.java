74f05674282f572393562a91e34fceec93dbb787
RealUGA <49420699+RealUGA@users.noreply.github.com>
2021-03-08 02:16:03 -0500

    Made consistent with main

--- src/myftp.java
+++ src/myftp.java
@@ -27,35 +27,77 @@ public class myftp {
 			String fullCommand;
 			String command;
 			String secondHalf;
-			String lastPart;
+			String endCommand;
 			while (check) {
 				System.out.print("mytftp>");
 				fullCommand = console.readLine();
 				int index = fullCommand.indexOf(" ");
 				int finalIndex = 0;
-				String endCommand = fullCommand;
+				endCommand = fullCommand;
 				int storedValue;
 				while (endCommand.indexOf(" ") > 0) {
 					storedValue = endCommand.indexOf(" ");
 					finalIndex += storedValue;
 					endCommand = endCommand.substring(storedValue + 1);
+					finalIndex += 1;
 				}
 				endCommand = fullCommand;
 				if (finalIndex <= 0) {
 					finalIndex = index;
 				} else {
-					endCommand = fullCommand.substring(finalIndex + 1); // will be used to check if ends in &
+					endCommand = fullCommand.substring(finalIndex); // will be used to check if ends in &
 				}
 				//System.out.println(index); // for testing
-				if (index < 0) {
-					command = fullCommand;
-					secondHalf = command;
+				if (endCommand.equals("&")) { // handles "&" cases
+	                fullCommand = fullCommand.substring(0,finalIndex - 1);
+	                if (index < 0) {
+						command = fullCommand;
+						secondHalf = command;
+					} else {
+						command = fullCommand.substring(0,index);
+						secondHalf = fullCommand.substring(index);
+					}
 				} else {
-					command = fullCommand.substring(0,index);
-					secondHalf = fullCommand.substring(index);
-				}
+					if (index < 0) {
+						command = fullCommand;
+						secondHalf = command;
+					} else {
+						command = fullCommand.substring(0,index);
+						secondHalf = fullCommand.substring(index);
+					}
+				}               
+                
 				if (command.equals("quit")) {
 					check = false;
+				} else if (command.equals("get") && endCommand.equals("&")) {
+					System.out.println("heller");
+					clientOutput.println(fullCommand + " " + endCommand);
+					String sizeString = clientInput.readLine();
+					if (sizeString.equals("false")) {
+						secondHalf = secondHalf.substring(1);
+						System.out.println(secondHalf + " does not exist in the current directory.");
+					} else {
+						long size = Long.parseLong(sizeString); // byte size of file
+						secondHalf = secondHalf.substring(1);
+						File test = new File(secondHalf);
+						int input = 0;
+						PrintWriter fTest = new PrintWriter(new BufferedWriter(new FileWriter(test)));
+						int i = 0;
+						while (input != -1) { // checks for end of file
+							input = clientInput.read();
+							if (input != -1) {
+								fTest.write(input);
+							}
+							i++;
+							if (i >= size) { // breaks if all bytes are read
+								break;
+							}
+						}
+						fTest.flush();
+						test.createNewFile();
+						fTest.close();
+						System.out.println(clientInput.readLine());
+					}
 				} else if (command.equals("get")) {
 					clientOutput.println(fullCommand);
 					String sizeString = clientInput.readLine();
@@ -84,6 +126,31 @@ public class myftp {
 						fTest.close();
 						System.out.println(clientInput.readLine());
 					}
+				} else if (command.equals("put") && endCommand.equals("&")) {
+					clientOutput.println(fullCommand + " " + endCommand);
+					secondHalf = secondHalf.substring(1);
+					File putFile = new File(secondHalf);
+					if (putFile.isFile()) {
+						BufferedReader bFileInput = new BufferedReader(new FileReader(putFile));
+						long fileLength = putFile.length();
+						String convert = "" + fileLength + "\n";
+						clientOutput.write(convert);
+						String output = "";
+						int value = 0;
+						while (value != -1) {
+							value = bFileInput.read();
+							if (value != -1) {
+								output += (char)value;
+							}
+						}
+						bFileInput.close();
+						clientOutput.println(output);
+						System.out.println(clientInput.readLine());
+					} else {
+						System.out.println(secondHalf + " does not exist in current directory.");
+						clientOutput.println("");
+						//System.out.println(clientInput.readLine());
+					}
 				} else if (command.equals("put")) {
 					clientOutput.println(fullCommand);
 					secondHalf = secondHalf.substring(1);
@@ -110,7 +177,8 @@ public class myftp {
 						//System.out.println(clientInput.readLine());
 					}
 				} else if (command.equals("terminate")) {
-					tOutput.println(fullCommand);
+					secondHalf = secondHalf.substring(1);
+					tOutput.println(secondHalf);
 					System.out.println(tInput.readLine()); // may need to be changed ----------------
 				} else {
 					clientOutput.println(fullCommand);
